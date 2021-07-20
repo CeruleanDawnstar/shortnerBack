25bca8ff10608218fee23961be90168ca823bb39
antoine <nogoup.antoine@gmail.com>
2021-07-20 23:02:51 +0200

    add the request to find all url

---
+++ bitly_back/src/api/models/urlLinks.js
@@ -0,0 +1,41 @@
+//const dbConn = require('../config/DataBaseHandler');
+const DataBaseHandler = require("../config/DataBaseHandler");
+const dataBaseHandler = new DataBaseHandler();
+const dbConn = dataBaseHandler.createConnection();
+
+
+const urlLink = function (link){
+    this.longLink = link.longUrl;
+    this.shortLink = link.shortUrl;
+    this.qrCode = link.qrCode;
+    this.title = link.title;
+    this.idUser = link.idUser;
+    this.dateLink = link.dateLink
+};
+
+urlLink.create = (newLinks, result)=>{
+    dbConn.query("INSERT INTO link SET ?", newLinks, (err, res)=>{
+        if(err){
+            console.log("error: ", err);
+            result(err, null);
+        }else{
+            console.log(res.insertedId)
+            result(null, {id: res.insertedId, ...newLinks });
+        }        
+    })
+}
+
+urlLink.findAll = (result)=>{
+    dbConn.query("SELECT idLink, longLink, shortLink FROM link", (err, res)=>{
+        if(err){
+            console.log("error: ", err);
+            result(err, null);
+        }else{
+            console.log('links : ', res)
+            result(null, res);
+        }
+    })
+}
+
+
+module.exports = urlLink;
