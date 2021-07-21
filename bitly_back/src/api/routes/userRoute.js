02faf7de19a390dd7365194f3d77ada0f5e7ca98
Cerulean <tahir.naseer@hotmail.fr>
2021-07-21 12:13:37 +0200

    added clean userRoute.js

--- bitly_back/src/api/routes/userRoute.js
+++ bitly_back/src/api/routes/userRoute.js
@@ -3,19 +3,6 @@ const router = express.Router()
 
 const userController = require('../controllers/userController');
 
-// // retrieve all users
-// router.get('/', userController.findAll);
-
-// // retrieve a single user with id
-// router.get('/:id', userController.findById);
-
-// // update a user with id
-// router.get('/:id', userController.update);
-
-// // delete a user with id
-// router.get('/:id', userController.delete);
-
-
 module.exports = (app) => {
     app.route('/users')
     .get(userController.findAll)
